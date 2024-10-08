e9a118813dc84efd506e7e18d1c6c6341dfccdd6
Renan Eberhardt <129560978+RenanEberhardt@users.noreply.github.com>
2024-10-08 20:38:04 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- aula 2/script.js
+++ aula 2/script.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
-    $.getJSON("./data.json", function(data){
-        alert(data.name);
+    $("#bt1").on("click", function(event){
+        $.getJSON("./data.json", function(data){
+            $("#resultado1").append(data.name + ", ");
+            $("#resultado1").append(data.age + " anos ");
+           
+            for(var i in data.cars){
+                $("#resultado1").append(data.cars[i]);
+            }
+            $("#resultado1").append("<br>");
+        });
+    });
+    $("#limpar").on("click", function(event){
+        $("div[id^=resultado]").empty();
     });
 });
