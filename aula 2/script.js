a3ae9259b30eabb9bb32acb8dc6a039ae036d8f7
Renan Eberhardt <129560978+RenanEberhardt@users.noreply.github.com>
2024-10-08 21:27:27 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- aula 2/script.js
+++ aula 2/script.js
@@ -3,14 +3,34 @@ $(document).ready(function(){
         $.getJSON("./data.json", function(data){
             $("#resultado1").append(data.name + ", ");
             $("#resultado1").append(data.age + " anos ");
-           
+            
             for(var i in data.cars){
                 $("#resultado1").append(data.cars[i]);
             }
             $("#resultado1").append("<br>");
         });
     });
+    $("#bt2").on("click", function(event){
+        $.getJSON("./data2.json", function(data){
+            $("#resultado2").append(data.name + " " + data.age + " anos:" "<br>");
+            
+            
+            for(var i in data.friends){
+                let friendName = data.friends[i].firstName + " " + data.friends[i].lastName;
+                $("#resultado2").append("Amigo " + (parseInt(i)+1) + ": " + friendName + "<br>");
+            }
+            $("#resultado2").append("Total de amigos de " + data.name + ": " + data.friends.length);
+        });
+    });
+
+    
+    
     $("#limpar").on("click", function(event){
         $("div[id^=resultado]").empty();
     });
+    
+    $("#limpar2").on("click", function(event){
+        $("div[id^=resultado2]").empty();
+    });
+    
 });
